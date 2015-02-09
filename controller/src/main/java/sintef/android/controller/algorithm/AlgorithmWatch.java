1af52405d0e7045de4d270be62ecd751f0732488
araneae <sondrehegdal@hotmail.com>
2015-02-09 14:03:41 +0100

    Added some stuff to the watch algorithm class

--- controller/src/main/java/sintef/android/controller/algorithm/AlgorithmWatch.java
+++ controller/src/main/java/sintef/android/controller/algorithm/AlgorithmWatch.java
@@ -10,12 +10,21 @@ import sintef.android.controller.sensor.SensorData;
  */
 public class AlgorithmWatch
 {
+    //TODO: find out how the sensor data should be read, so that it can be used in the Fall Index method.
 
+    //call it something better when I find something better to name it
+    //the main method of the algorithm. Will allway run (or sleep), and does the fetching of sensordata, and runs the other methods when needed.
+    public void fallAlgorithm ()
+    {
+        //TODO: make this.
+    }
+
+    //Calculate the acceleration.
     private double fallIndex (List<SensorData> sensors)
     {
-        List <Double> x = sensors.get(0);
-        List <Double> y = sensors.get(0);
-        List <Double> z = sensors.get(0);
+        List <Double> x = (List<Double>) sensors.get(0);
+        List <Double> y = (List<Double>) sensors.get(0);
+        List <Double> z = (List<Double>) sensors.get(0);
 
         List <List> sensorData = new ArrayList<List>();
         sensorData.add(x);
@@ -24,7 +33,7 @@ public class AlgorithmWatch
 
         double directionAcceleration = 0;
         double totAcceleration = 0;
-        double result = 0;
+        double result;
 
         for (int i = 0; i < sensorData.size(); i++)
         {
@@ -32,9 +41,9 @@ public class AlgorithmWatch
             {
                 if (j > 0)
                 {
-                    directionAcceleration += Math.pow(sensorData.get(i).get(j) - sensorData.get(i).get(j - 1), 2);
+                    directionAcceleration += Math.pow((Double)sensorData.get(i).get(j) - (Double)sensorData.get(i).get(j - 1), 2);
                 }
-                //not sure if this one is necessary
+                //not sure if this one is necessary/destroys the algorithm
                 else
                 {
                     directionAcceleration = Math.pow((Double) sensorData.get(i).get(j), 2);
@@ -44,7 +53,17 @@ public class AlgorithmWatch
             directionAcceleration = 0;
         }
         result = Math.sqrt(totAcceleration);
-        
+
         return result;
+    }
+
+
+    //might change the name/remove this if necessary, might want to change the parameter
+    //Recognize fall pattern, and decide if there is a fall or not
+    public boolean patternRecognition (double accelerationData)
+    {
+        //TODO: make this
+        double threshold = 0;
+        return accelerationData >= threshold;
     }
 }
