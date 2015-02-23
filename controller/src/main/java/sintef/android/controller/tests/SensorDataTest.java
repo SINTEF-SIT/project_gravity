a7dc6ce610e755a0fedb922b1c503a9dbd8395e6
olehalvor <olli408@hotmail.com>
2015-02-23 11:18:12 +0100

    new test3 branch

---
+++ controller/src/main/java/sintef/android/controller/tests/SensorDataTest.java
@@ -0,0 +1,30 @@
+package sintef.android.controller.tests;
+
+import android.test.InstrumentationTestCase;
+
+import junit.framework.TestCase;
+
+import sintef.android.controller.sensor.SensorData;
+import sintef.android.controller.sensor.SensorDevice;
+import sintef.android.controller.sensor.SensorLocation;
+import sintef.android.controller.sensor.SensorSession;
+import sintef.android.controller.sensor.data.SensorDataObject;
+
+/**
+ * Created by Ole on 19.02.2015.
+ */
+public class SensorDataTest extends InstrumentationTestCase {
+
+    @Override
+    protected void setUp() throws Exception{
+        super.setUp();
+    }
+
+    public void testSensorData (){
+    }
+
+    @Override
+    protected void tearDown() throws Exception{
+        super.tearDown();
+    }
+}
