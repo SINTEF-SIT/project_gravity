5087d3496abb16e07946abc9a659d48a2087d0d9
Daniel <daniel_bajer@hotmail.com>
2015-02-26 12:51:18 +0100

    testdaniel

--- controller/src/main/java/sintef/android/controller/tests/IntegrationTesting.java
+++ controller/src/main/java/sintef/android/controller/tests/IntegrationTesting.java
@@ -8,7 +8,6 @@ import sintef.android.controller.sensor.SensorData;
 import sintef.android.controller.sensor.SensorDevice;
 import sintef.android.controller.sensor.SensorLocation;
 import sintef.android.controller.sensor.SensorSession;
-import sintef.android.controller.sensor.data.AccelerometerData;
 import sintef.android.controller.sensor.data.GravityData;
 
 /**
@@ -16,28 +15,30 @@ import sintef.android.controller.sensor.data.GravityData;
  */
 public class IntegrationTesting extends InstrumentationTestCase {
 
-    SensorData data11;
-    SensorData data2;
+    SensorData retrieveData;
+    SensorData sendData;
 
     public void testMain(){
         EventBus.getDefault().registerSticky(this);
-        SensorSession session1 = new SensorSession("id1", Sensor.TYPE_ACCELEROMETER, SensorDevice.WATCH , SensorLocation.LEFT_PANT_POCKET);
-        float[] liste = new float[1];
-        liste[0] = 2;
-        GravityData data1 = new GravityData(liste);
-        data2 = new SensorData(session1, data1, System.currentTimeMillis());
-        EventBus.getDefault().post(data2);
+        SensorSession session = new SensorSession("id1", Sensor.TYPE_ACCELEROMETER, SensorDevice.WATCH , SensorLocation.LEFT_PANT_POCKET);
+        float[] list = new float[1];
+        list[0] = 2;
+        GravityData origData;
+        origData = new GravityData(list);
+        sendData = new SensorData(session, origData, System.currentTimeMillis());
+        EventBus.getDefault().post(sendData);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        assertEquals(data11,data2);
+        assertEquals(retrieveData, sendData);
     }
+    // Should listen to isFall instead of directly from eventbus, kan da vente en bestemt tid
 
     public void onEvent(SensorData data){
-        data11 = data;
+        retrieveData = data;
 
     }
 
