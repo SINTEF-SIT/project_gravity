9e37a33c8247f111aa79a48a9b3d411d17a64cfa
andlon93 <andreas.lones@gmail.com>
2015-03-09 13:58:24 +0100

    made test methd for impact algorithm and edited test values

--- controller/src/main/java/sintef/android/controller/algorithm/AlgorithmPhone.java
+++ controller/src/main/java/sintef/android/controller/algorithm/AlgorithmPhone.java
@@ -14,6 +14,7 @@ public class AlgorithmPhone
     private static double verticalAccThreshold = 8;
     private static double accComparisonThreshold = 0.75;
     private static double angleThreshold = 20;
+    private static double impactThreshold = 3;
 
     public static boolean isFall(double x, double y, double z, double tetaY, double tetaZ)
     {
@@ -50,10 +51,12 @@ public class AlgorithmPhone
     if there is a massive deaccelreation close after the max acceleration impact indicates that it is a fall.
 
         Test values == TRUE:
+    impactThreshold = 3
     x = [3, 4, 2, 6, 4, 4, 2]
     y = [3, 4, 2, 6, 4, 3, 1]
     z = [3, 4, 2, 6, 4, 4, 2]
         Test values == FALSE:
+    impactThreshold = 3
     x = [6, 5, 5, 4, 4, 3, 0]
     y = [6, 5, 5, 4, 3, 3, 0]
     z = [6, 5, 5, 4, 3, 3, 0]
@@ -63,7 +66,7 @@ public class AlgorithmPhone
         int counter = 0;
         final int iterationsAfterMaxAcc = 5;
         double currentAcceleration;
-        double threshold1 = 3;
+
 
         for (int i = index; i < accelerometerData.size(); i++){
             currentAcceleration = accelerationTotal(accelerometerData.get(i).getX(), accelerometerData.get(i).getY(), accelerometerData.get(i).getY());
@@ -73,13 +76,34 @@ public class AlgorithmPhone
             }
             else {
                 if(counter == 0){return false;}
-                if(currentAcceleration*threshold1 < maxAcceleration){return true;}
+                if(currentAcceleration*impactThreshold < maxAcceleration){return true;}
                 counter --;
             }
         }
         return false;
     }
+    //FOR TESTING PURPOSES
+    public boolean impactPattern(List<AccelerometerData> accelerometerData, int index, double impactThreshold){
+        double maxAcceleration = 0;
+        int counter = 0;
+        final int iterationsAfterMaxAcc = 5;
+        double currentAcceleration;
 
+
+        for (int i = index; i < accelerometerData.size(); i++){
+            currentAcceleration = accelerationTotal(accelerometerData.get(i).getX(), accelerometerData.get(i).getY(), accelerometerData.get(i).getY());
+            if (currentAcceleration > maxAcceleration){
+                maxAcceleration = currentAcceleration;
+                counter = iterationsAfterMaxAcc;
+            }
+            else {
+                if(counter == 0){return false;}
+                if(currentAcceleration*impactThreshold < maxAcceleration){return true;}
+                counter --;
+            }
+        }
+        return false;
+    }
     public static boolean isPhoneVertical(double priorAngle, double postAngle, double angleThreshold)
     {
         if (postAngle - priorAngle >= angleThreshold){ return true; }
