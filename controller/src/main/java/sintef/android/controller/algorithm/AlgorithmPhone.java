4b35f5b007fcaf2c923c1c508fec1cb58a330998
andlon93 <andreas.lones@gmail.com>
2015-03-09 13:06:43 +0100

    worked on impact pattern recognition

--- controller/src/main/java/sintef/android/controller/algorithm/AlgorithmPhone.java
+++ controller/src/main/java/sintef/android/controller/algorithm/AlgorithmPhone.java
@@ -3,6 +3,7 @@ package sintef.android.controller.algorithm;
 import java.util.List;
 
 import sintef.android.controller.sensor.SensorData;
+import sintef.android.controller.sensor.data.AccelerometerData;
 
 /**
  * Created by Andreas on 10.02.2015.
@@ -44,6 +45,30 @@ public class AlgorithmPhone
         return false;
     }
 
+    /*
+    impact pattern recognition:
+    if there is a massive deaccelreation close after the max acceleration impact indicates that it is a fall.
+     */
+    public boolean impactPattern(List<AccelerometerData> accelerometerData, int indeks){
+        double maxAcceleration = 0;
+        int counter = 0;
+        double currentAcceleration;
+        double threshold1 = 5;
+
+        for (int i = indeks; indeks < accelerometerData.size(); i++){
+            currentAcceleration = accelerationTotal(accelerometerData.get(i).getX(), accelerometerData.get(i).getY(), accelerometerData.get(i).getY());
+            if (currentAcceleration > maxAcceleration){
+                maxAcceleration = currentAcceleration;
+                counter = 10;
+            }
+            else {
+                if(counter != 0 && currentAcceleration*threshold1 < maxAcceleration){return true;}
+                counter --;
+            }
+        }
+        return false;
+    }
+
     public static boolean isPhoneVertical(double priorAngle, double postAngle, double angleThreshold)
     {
         if (postAngle - priorAngle >= angleThreshold){ return true; }
@@ -62,7 +87,7 @@ public class AlgorithmPhone
     }
 
     //calculates vertical acceleration at one point
-        private static double verticalAcceleration(double x, double y, double z, double tetaY, double tetaZ)
+    private static double verticalAcceleration(double x, double y, double z, double tetaY, double tetaZ)
     {
         return x*Math.sin(tetaZ) + y*Math.sin(tetaY) - z*Math.cos(tetaY)*Math.cos(tetaZ);
     }
