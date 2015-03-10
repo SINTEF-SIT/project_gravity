087db5fa888fc8b3961aa75e60bf8aae43ee61aa
imaltont <sondrehegdal@hotmail.com>
2015-03-10 12:15:48 +0100

    Simple pattern recognition for laying still after the fall

--- controller/src/main/java/sintef/android/controller/algorithm/AlgorithmWatch.java
+++ controller/src/main/java/sintef/android/controller/algorithm/AlgorithmWatch.java
@@ -14,20 +14,23 @@ public class AlgorithmWatch
     private static final double thresholdFall = 2;
     //private static final double thresholdImpact = 8;
     private static final double thresholdStill = 3;
+    private static  final double gravity = 9.81;
 
     //Calculate the acceleration.
     //Switch back to List <SensorData> after testing
-    private static double fallIndex(List<AccelerometerData> sensors)
+    private static double fallIndex(List<AccelerometerData> sensors, int startXYZ, int startList)
     {
 
         List <Double> x = new ArrayList<>();
         List <Double> y = new ArrayList<>();
         List <Double> z = new ArrayList<>();
 
+        double fall = 0;
+
         for (AccelerometerData xyz : sensors)
         {
             x.add((double) xyz.getX());
-            y.add((double) xyz.getY());
+            y.add((double) xyz.getY()-gravity);
             z.add((double) xyz.getZ());
         }
 
@@ -45,6 +48,12 @@ public class AlgorithmWatch
             for (int j = 1; j < sensorData.get(i).size(); j++)
             {
                 directionAcceleration += Math.pow((Double)sensorData.get(i).get(j) - (Double)sensorData.get(i).get(j - 1), 2);
+                if (Math.pow((Double)sensorData.get(i).get(j) - (Double)sensorData.get(i).get(j - 1), 2) > fall)
+                {
+                    fall = Math.pow((Double)sensorData.get(i).get(j) - (Double)sensorData.get(i).get(j - 1), 2);
+                    startXYZ = i;
+                    startList = j;
+                }
             }
             totAcceleration += directionAcceleration;
             directionAcceleration = 0;
@@ -90,6 +99,11 @@ public class AlgorithmWatch
         return Math.sqrt(totAcceleration);
     }
 
+    private static double stillPattern(List<AccelerometerData> sensors, int startXYZ, int startList)
+    {
+        return fallIndex(sensors, startXYZ, startList);
+    }
+
 
     //might change the name/remove this if necessary, might want to change the parameter
     //Recognize fall pattern, and decide if there is a fall or not
@@ -98,27 +112,17 @@ public class AlgorithmWatch
         double accelerationData;
         //double impactFallData;
         double afterFallData;
+        int startXYZ = 0;
+        int startList = 0;
 
-        if (sensors.size() >= 20) accelerationData = fallIndex(sensors, 20);
-        else accelerationData = fallIndex(sensors);
+        //if (sensors.size() >= 20) accelerationData = fallIndex(sensors, 20);
+        accelerationData = fallIndex(sensors, startXYZ, startList);
 
         if (accelerationData > thresholdFall)
         {
-            return true;
-            //TODO: fix pattern recognition
-            /*afterFallData = fallIndex(sensors, 5);
-            if (afterFallData < thresholdStill)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }*/
+            afterFallData = stillPattern(sensors, startXYZ, startList);
+            return afterFallData < thresholdStill;
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 }
