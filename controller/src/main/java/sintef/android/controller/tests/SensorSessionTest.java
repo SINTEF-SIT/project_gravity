69bc52f8917a19000a502b6240d68811a51be16a
samyboy89 <sam.mathias.weggersen@gmail.com>
2015-05-23 11:29:03 +0200

    Fixed test, Added new apk

--- controller/src/main/java/sintef/android/controller/tests/SensorSessionTest.java
+++ controller/src/main/java/sintef/android/controller/tests/SensorSessionTest.java
@@ -22,8 +22,6 @@ package sintef.android.controller.tests;
 import android.hardware.Sensor;
 import android.test.InstrumentationTestCase;
 
-import junit.framework.TestCase;
-
 import sintef.android.controller.sensor.SensorDevice;
 import sintef.android.controller.sensor.SensorLocation;
 import sintef.android.controller.sensor.SensorSession;
@@ -34,26 +32,21 @@ import sintef.android.controller.sensor.SensorSession;
 public class SensorSessionTest extends InstrumentationTestCase {
 
     @Override
-    protected void setUp() throws Exception{
+    protected void setUp() throws Exception {
         super.setUp();
     }
 
-    public void testSession (){
+    public void testSensorSession() {
         SensorSession session1 = new SensorSession("id1", Sensor.TYPE_ACCELEROMETER, SensorDevice.WATCH , SensorLocation.LEFT_PANT_POCKET);
-        assertEquals(session1.getId(),"id1");
-        assertEquals(session1.getSensorDevice(),SensorDevice.WATCH);
-        assertEquals(session1.getSensorType(),1);
-        assertEquals(session1.getSensorLocation(),SensorLocation.LEFT_PANT_POCKET);
+        assertEquals(session1.getId(), "id1");
+        assertEquals(session1.getSensorDevice(), SensorDevice.WATCH);
+        assertEquals(session1.getSensorType(), 1);
+        assertEquals(session1.getSensorLocation(), SensorLocation.LEFT_PANT_POCKET);
         assertEquals(session1,session1.getSessionFromString(session1.getStringFromSession()));
-
-
-
     }
 
-
-
     @Override
-    protected void tearDown() throws Exception{
+    protected void tearDown() throws Exception {
         super.tearDown();
     }
 }
